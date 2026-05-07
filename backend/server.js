3b60504d0481d1343983349791989c8ac8334e83
Mohit <mt9430834@gmail.com>
2026-05-07 16:20:57 +0530

    fixed server routes

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const connectDB = require("./src/config/db");
+
 const authRoutes = require("./src/routes/authRoutes");
 const projectRoutes = require("./src/routes/projectRoutes");
 const taskRoutes = require("./src/routes/taskRoutes");
 const dashboardRoutes = require("./src/routes/dashboardRoutes");
+
 dotenv.config();
 
 connectDB();
@@ -15,14 +17,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.get("/", (req, res) => {
 
-    res.status(200).json({
-        success: true,
-        message: "Team Task Manager API Running"
-    });
-
-});
 app.use(cors({
     origin: "*",
     credentials: true
@@ -31,17 +26,19 @@ app.use(cors({
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("Team Task Manager API Running");
+    res.status(200).json({
+        success: true,
+        message: "Team Task Manager API Running"
+    });
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
-});
-app.get("/", (req, res) => {
-    res.send("Backend Running");
 });
