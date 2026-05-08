3099ba2347ded3f816425c871f73a03aa9a152aa
Mohit <mt9430834@gmail.com>
2026-05-08 10:20:54 +0530

    Fixed missing task routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./src/config/db");
 
 const authRoutes = require("./src/routes/authRoutes");
 const projectRoutes = require("./src/routes/projectRoutes");
+const taskRoutes = require("./src/routes/taskRoutes");
 const dashboardRoutes = require("./src/routes/dashboardRoutes");
 
 dotenv.config();
@@ -33,6 +34,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
